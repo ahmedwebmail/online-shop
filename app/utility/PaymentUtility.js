09f8130f5c49f7cc7da38c56ef32555a887ca4e6
cyber khalid <147651505+ahmedwebmail@users.noreply.github.com>
2025-09-23 19:37:48 +0100

    inital commit

---
+++ app/utility/PaymentUtility.js
@@ -0,0 +1,19 @@
+export const payment_init = async()=>{
+
+}
+
+export const payment_success = async()=>{
+
+}
+
+export const payment_fail = async()=>{
+
+}
+
+export const payment_cancel = async()=>{
+
+}
+
+export const payment_notification = async()=>{
+
+}
